747b8d03e086e5c3b2ea55ca3910a2319a28a9be
Monika Rovt <mirarooti@gmail.com>
2026-05-18 01:05:33 +0300

    add 4 task to js-7

--- js/task-4.js
+++ js/task-4.js
@@ -2,13 +2,22 @@ const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", event => {
 	event.preventDefault();
-  const email = form.elements.email.value;
-  const password = form.elements.password.value;
-  
-  if (email === "" || password === "") {
-    return alert('All form fields must be filled in');
+ const { elements } = event.currentTarget;
+  const email = elements.email.value.trim();
+  const password = elements.password.value.trim();
+
+  if (!email || !password) {
+    alert('All form fields must be filled in');
+    return;
   }
-   console.log(`Email: ${email}, Password: ${password}`);
-   form.reset();
+
+  const formData = {
+    email: email,
+    password: password
+  };
+
+  console.log(formData); 
+
+  event.currentTarget.reset(); 
 });
 
